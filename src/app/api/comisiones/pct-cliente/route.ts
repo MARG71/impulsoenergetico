eff3cca482311cddc45737f470298f3c21c4a8b0
MARG1971 <infolatiendadelasgangas@gmail.com>
2025-12-09 20:50:49 +0100

    arreglo error vercel

--- src/app/api/comisiones/pct-cliente/route.ts
+++ src/app/api/comisiones/pct-cliente/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";  // 👈 AQUÍ EL CAMBIO
+
 
 export async function GET(req: NextRequest) {
   try {
