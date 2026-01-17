434157c79d02684931736c2d15018244a75f1640
MARG1971 <infolatiendadelasgangas@gmail.com>
2026-01-17 21:12:02 +0100

    arregloe errorres

--- src/app/(crm)/leads/[id]/page.tsx
+++ src/app/(crm)/leads/[id]/page.tsx
@@ -1,11 +1,6 @@
 // src/app/(crm)/leads/[id]/page.tsx
-import { Suspense } from "react";
 import LeadDetalleContenido from "./LeadDetalleContenido";
 
 export default function LeadDetallePage() {
-  return (
-    <Suspense fallback={<div className="p-6 text-slate-100">Cargando lead…</div>}>
-      <LeadDetalleContenido />
-    </Suspense>
-  );
+  return <LeadDetalleContenido />;
 }
--- src/app/(crm)/leads/page.tsx
+++ src/app/(crm)/leads/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import LeadsContenido from "./LeadsContenido";
 
 export default function LeadsPage() {
-  return (
-    <Suspense fallback={<div className="p-6 text-slate-100">Cargando leads…</div>}>
-      <LeadsContenido />
-    </Suspense>
-  );
+  return <LeadsContenido />;
 }
